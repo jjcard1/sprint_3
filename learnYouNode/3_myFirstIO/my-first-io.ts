5c98d3cced7ba9bee61170202344ad011d0308a5
Juanjo <juanjo.card.m@gmail.com>
2023-09-13 11:11:10 +0200

    learnyounode  4

--- learnYouNode/3_myFirstIO/my-first-io.ts
+++ learnYouNode/3_myFirstIO/my-first-io.ts
@@ -1,8 +1,9 @@
-const fs = require('fs')
+const fss = require('fs')
 
 const countLinesFile = () => {
     const pathFile:string = process.argv[2]
-    const contain:string = fs.readFileSync(pathFile, 'utf8')
+    console.log(pathFile)
+    const contain:string = fss.readFileSync(pathFile, 'utf8')
     const countLines = contain.split('\n').length - 1
     return countLines
 }
--- learnYouNode/4_myFirstAsync/my-first-async-io.ts
+++ learnYouNode/4_myFirstAsync/my-first-async-io.ts
@@ -1,11 +1,10 @@
 const fs = require('fs')
 
-const countLinesAsync = () => {
-    const pathFile = process.argv[2]
-    const readFileAsync = fs.readFile(pathFile, 'utf8')
-    const numberLines = ''
-    return numberLines
-}
+fs.readFile(process.argv[2], function(err:any, data:any) {
+    if (!err) {
+        const text = data.toString();
+        const arraytext = text.split('\n');
+        console.log(arraytext.length - 1);
+    };
+});
 
-const resultCountAsync = countLinesAsync()
-console.log(resultCountAsync)
